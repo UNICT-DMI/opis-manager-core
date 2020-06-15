bb90046d64a2134700da1f54ebe1e35233012047
lemuelpuglisi <lemuelpuglisi001@gmail.com>
2020-06-15 23:06:30 +0200

    feat: add tables migrations

---
+++ database/migrations/2020_06_15_194355_create_insegnamento_table.php
@@ -0,0 +1,127 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInsegnamentoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('insegnamento', function (Blueprint $table) {
+            
+            /**
+             * Identificativo progressivo 
+             */
+            $table->id(); 
+            
+            /**
+             * codice gomp dell'insegnamento
+             */
+            $table->integer('codice_gomp', false); 
+
+            /**
+             * Nome dell'insegnamento
+             */
+            $table->string('nome'); 
+
+            /**
+             * Anno accademico di inserimento
+             */
+            $table->string('anno_accademico');  
+
+            /**
+             * campo di ricerca 
+             */
+            $table->string('ssd');
+            
+            /**
+             * Anno a partire dal quale si ha accesso all'insegnamento
+             */
+            $table->string('anno'); 
+
+            /**
+             * Semestre di appartenenza 
+             */
+            $table->string('semestre'); 
+
+            /**
+             * crediti assegnati alla materia
+             */
+            $table->string('cfu'); 
+
+            /**
+             * docente assegnato alla materia
+             */
+            $table->string('docente')
+                ->nullable()
+                ->default(NULL);
+
+            /**
+             * Nel caso il corso sia suddiviso in canali, 
+             * si riporta il canale di riferimento
+             */
+            $table->string('canale')
+                ->nullable()
+                ->default(''); 
+            
+            /**
+             * Nel caso l'insegnamento disponga di più moduli, 
+             * si riporta il modulo di riferimento
+             */
+            $table->string('modulo')
+                ->nullable()
+                ->default(''); 
+            
+            /**
+             * ??
+             */
+            $table->string('tipo')
+                ->nullable()
+                ->default(NULL); 
+            
+            /**
+             * ??
+             */
+            $table->string('assegn')
+                ->nullable()
+                ->default(NULL); 
+
+            /**
+             * Identificativo del corso di studi di appartenenza
+             */
+            $table->unsignedBigInteger('id_cds'); 
+
+            $table->foreign('id_cds')
+                ->references('id')->on('corso_di_studi');
+
+            /**
+             * L'identificativo fornito dall'unict, insieme 
+             * all'anno accademico, forniscono una chiave univoca
+             * abbastanza scomoda da utilizzare. Tuttavia, è
+             * necessario garantirne l'univocità. 
+             */
+            $table->unique([
+                'codice_gomp', 
+                'anno_accademico', 
+                'modulo', 
+                'canale'
+            ]);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('insegnamento');
+    }
+}

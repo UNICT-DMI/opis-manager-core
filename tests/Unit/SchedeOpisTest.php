969dcb3ee370ba491a927a3339995d26db1b9a66
lemuelpuglisi <lemuelpuglisi001@gmail.com>
2020-06-19 23:08:32 +0200

    test: test schede opis model relations

---
+++ tests/Unit/SchedeOpisTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use App\Models\SchedeOpis;
+use App\Models\Insegnamento;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class SchedeOpisTest extends TestCase
+{
+    use RefreshDatabase; 
+
+    /** @test */
+    public function can_get_insegnamento(): void
+    {
+        $this->seed(\DipartimentoTableSeeder::class);
+        $this->seed(\CorsoDiStudiTableSeeder::class);
+        $this->seed(\SchedeOpisTableSeeder::class);
+
+        $insegnamento = Insegnamento::first(); 
+        $schedeOpis = SchedeOpis::first(); 
+
+        $schedeOpis->id_insegnamento = $insegnamento->id; 
+        $schedeOpis->save(); 
+
+        $this->assertEquals($schedeOpis->id_insegnamento, $insegnamento->id); 
+    }
+
+    /** @test */
+    public function insegnamento_attribute_is_instance_of_insegnamento(): void
+    {
+        $this->seed(\DipartimentoTableSeeder::class);
+        $this->seed(\CorsoDiStudiTableSeeder::class);
+        $this->seed(\SchedeOpisTableSeeder::class);
+        $this->assertTrue(SchedeOpis::first()->insegnamento instanceof Insegnamento); 
+    }
+
+    /** @test */
+    public function corso_di_studi_function_is_instance_of_belongsTo(): void
+    {
+        $this->seed(\DipartimentoTableSeeder::class);
+        $this->seed(\CorsoDiStudiTableSeeder::class);
+        $this->seed(\SchedeOpisTableSeeder::class);
+        $this->assertTrue(SchedeOpis::first()->insegnamento() instanceof BelongsTo); 
+    }
+
+}

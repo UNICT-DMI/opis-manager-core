6c840c5beb6c011bc27b20a1095562e46df104cd
Charlemagne <lemuelpuglisi001@gmail.com>
2020-06-21 17:26:44 +0200

    feat: add opis models find helper

---
+++ app/Traits/OpisModelFinder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Traits; 
+
+use Illuminate\Database\Eloquent\Model;
+
+trait OpisModelFinder
+{
+    /**
+     * Trova un modello opis attraverso l'id fornito dal sito 
+     * dell'unict (unict_id) e l'anno accademico di riferimento.
+     * Se non viene trovato alcun modello, scatena una exception
+     * http not-found che ritornerà una response con status code
+     * 404 al client.  
+     *
+     * @param  int $unictId
+     * @param  string $annoAccademico
+     * @return Model
+     */
+    public static function opisFindOrFail(int $unictId, string $annoAccademico): Model
+    {
+        $model = self::where('unict_id', $unictId)
+            ->where('anno_accademico', $annoAccademico)
+            ->first(); 
+
+        if ($model === null) 
+            abort(404); 
+        
+        return $model; 
+    }
+}

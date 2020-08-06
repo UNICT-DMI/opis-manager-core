434087c45e604a70b247b0289a5e407a20c6844f
lemuelpuglisi <lemuelpuglisi001@gmail.com>
2020-08-06 19:56:53 +0200

    fix: sanitize answer arrays elements

--- app/Http/Resources/StructuredSchedeOpis.php
+++ app/Http/Resources/StructuredSchedeOpis.php
@@ -15,11 +15,15 @@ class StructuredSchedeOpis extends JsonResource
     public function toArray($request)
     {
         $resourceToArray = parent::toArray($request); 
+        $domandeArray = json_decode($resourceToArray['domande'], true);
+
+        // per incertezza sulla provenienza dei dati, viene convertito ogni elemento
+        // ad un valore intero
+        $domandeArray = array_map('intval', $domandeArray); 
 
         // le domande sono conservate in json contenente un array da 55 valori. 
         // Il client richiede che tale array sia diviso in 11 array da 5 valori
         // ciascuno. 
-        $domandeArray  = json_decode($resourceToArray['domande'], true);
         $splittedArray = array_chunk($domandeArray, 5);
 
         $resourceToArray['domande'] = \json_encode($splittedArray);  

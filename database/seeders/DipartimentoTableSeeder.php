96b8c4fba72aa6317fc312ff7aed27c49acb0ed3
Lemuel <42694683+LemuelPuglisi@users.noreply.github.com>
2021-07-12 12:36:25 +0200

    refactor: fix breaking changes from migration to laravel 8 (#40)
    * refactor: fix breaking changes from migration to laravel 8
    
    * fix: add ignore-platform-reqs on remote tests

--- database/seeders/DipartimentoTableSeeder.php
+++ database/seeders/DipartimentoTableSeeder.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace Database\Seeders;
+
 use App\Models\Dipartimento;
 use Illuminate\Database\Seeder;
 
--- database/seeders/InsegnamentoCanaliSeeder.php
+++ database/seeders/InsegnamentoCanaliSeeder.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace Database\Seeders;
+
 use App\Models\CorsoDiStudi;
 use App\Models\Insegnamento;
 use Illuminate\Database\Seeder;
